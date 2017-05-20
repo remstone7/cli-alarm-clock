928fd1c1f44baf4a258d5c4fdcade3ef516bbc59
Rem Stone <remstone7@gmail.com>
2017-05-19 23:00:38 -0400

    open song after x seconds

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,4 +1,4 @@
-# put some youtube links in text file
+# put some youtube links in json file
 #     program will read the file.
 # You have to set time whatever you want in any format
 #     and at that particular time
@@ -6,10 +6,31 @@
 #     and start playing youtube video
 
 import json
-
+import random
+import time
+import webbrowser
 song_list = []
+
+def main():
+    set_time_from_now()
 
 with open('sound.json') as data_file:
     data = json.load(data_file)
     for s in data['songs']:
         song_list.append(s['url'])
+
+
+def set_time_from_now():
+    user_input = input('How long would you like until music goes off (seconds): ')
+    user_input = int(user_input)
+    time.sleep(user_input)
+
+    play_song()
+
+def play_song():
+    song = random.choice(song_list)
+    webbrowser.open(song)
+
+
+if __name__ == '__main__':
+    main()
